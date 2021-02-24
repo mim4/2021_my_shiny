94ed2f7c45331bb1c4b97ef4958ef17878912bfd
mim4 <milundain98@gmail.com>
2021-02-24 14:57:33 +0100

    Title well written

--- my_shiny/app.R
+++ my_shiny/app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("This is a ner Shiny App"),
+    titlePanel("This is a new Shiny App"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
